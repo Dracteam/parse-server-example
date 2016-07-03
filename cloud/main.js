626598b56ec92aa86675af14c1c8c12256621887
dambrosini92 <dambrosini@unocad.it>
2016-07-03 11:35:04 +0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,7 +2,21 @@ var Mailgun = require('mailgun');
 Mailgun.initialize(process.env.MAILGUN_DOMAIN , process.env.MAILGUN_API_KEY);
 
 Parse.Cloud.define('hello', function(req, res) {
-res.success('Hello ! Cloud Code Work Fine, mailgun domain: '+ process.env.MAILGUN_DOMAIN + 'api key: '+  process.env.MAILGUN_API_KEY  +  'from: ' +  process.env.TESTMAIL + 'to:  ' + process.env.MAILGUN_SMTP_LOGIN);
+Mailgun.sendEmail({
+    to: process.env.TESTMAIL,
+    from: process.env.MAILGUN_SMTP_LOGIN,
+    subject: "Hello from Cloud Code!",
+    text: "Using Parse and Mailgun is great!"
+}, {
+    success: function(httpResponse) {
+        console.log(httpResponse);
+        res.success("Email sent!");
+    },
+    error: function(httpResponse) {
+       console.error(httpResponse);
+       res.error("Uh oh, something went wrong");
+    }
+});
 });
 
 
