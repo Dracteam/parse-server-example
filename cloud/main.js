fe507e103593c1a45627bbe1b26a8634769c7dcb
dambrosini92 <dambrosini@unocad.it>
2016-07-03 15:44:09 +0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -27,13 +27,13 @@ Parse.Cloud.define("sendMail", function(request, response) {
         method: "POST",
         url: "https://api:" + process.env.MAILGUN_API_KEY + "@api.mailgun.net/v2/" + process.env.MAILGUN_DOMAIN + "/messages",
         body: { 
-      to: process.env.TESTMAIL, 
-      from: process.env.MAILGUN_SMTP_LOGIN, 
-      subject: "Hello from Cloud Code!", 
-      text: "Using Parse and Mailgun is great!" 
-      
-      },success: function(httpResponse) {
-        response(Email Sent!);
+            to: process.env.TESTMAIL, 
+            from: process.env.MAILGUN_SMTP_LOGIN, 
+            subject: "Hello from Cloud Code!", 
+            text: "Using Parse and Mailgun is great!" 
+      },
+      success: function(httpResponse) {
+        response('Email Sent!');
       },
       error: function(httpResponse) {
         response('Request failed with response code ' + httpResponse.status)
