6807cd7967a19e488c00f004035230362368cf20
dambrosini92 <dambrosini@unocad.it>
2016-07-03 10:53:16 +0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,5 @@
 Parse.Cloud.define('hello', function(req, res) {
-res.success('Hello ! Cloud Code Work Fine, mailgun domain:' + process.env.MAILGUN_DOMAIN + 'mailgun api key' + process.env.MAILGUN_API_KEY + 'to:'  process.env.TESTMAIL + 'from' + process.env.MAILGUN_SMTP_LOGIN);
+res.success('Hello ! Cloud Code Work Fine, mailgun domain:'+  process.env.TESTMAIL + 'from' + process.env.MAILGUN_SMTP_LOGIN);
 });
 
 
