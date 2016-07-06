315c6835fdc0b689e092db98d2c0d0e23c2180d7
dambrosini92 <dambrosini@unocad.it>
2016-07-06 16:55:01 +0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,30 +1,25 @@
 Parse.Cloud.define("downloaditems", function(request, response){
    var items;
    var query = new Parse.Query("Items");
-
-    var promise = new Parse.Promise();
-
-    query.find().then(function(results) {
-        if (results.length == 0)
-        {
-            promise.resolve("No items found!");
-        }
-        else
-        {
-           items = results;    
-           promise.resolve(results);
-            
-        }
-    }).then(function() {
-        response.success = items;
-    }, function(error) {
-       response.error(error);
-    });
-
-    return promise;
-    
-    
-});
+ 
+     var promise = new Parse.Promise();
+ 
+     query.find().then(function(results) {
+         if (results.length == 0)
+         {
+             promise.resolve("No items found!");
+         }
+         else
+         {
+            items = results;    
+            promise.resolve(results);
+             
+         }
+     }).then(function() {
+         response.success(items);
+     }, function(error) {
+        response.error(error);
+      });
 
 
 Parse.Cloud.define("purchase", function(request, response) {
