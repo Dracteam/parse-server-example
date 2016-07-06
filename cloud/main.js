e93c83f3e7c5862bd11e14c11233242eacd90b6d
dambrosini92 <dambrosini@unocad.it>
2016-07-06 12:14:30 +0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -7,14 +7,9 @@ Parse.Cloud.define("purchase", function(request, response) {
  // Query of items
   Parse.Promise.as().then(function(){
                           
-    var itemsQuery = new Parse.Query('Items');                      
-    return itemsQuery.find().then(null, function(error){
-        console.error('Request failed with response code ' + error);
-    return Parse.Promise.error('1 - Sorry, an error occurred.');                          
-    });                      
-}).then(function(results){
+    var query = new Parse.Query('Items');                      
+    return itemsQuery.find().then(function(results){
     if (!results) {
-        console.error('Request failed with response code ' + error);
       return Parse.Promise.error('2 - Sorry, an error occurred.');
     } 
     var itemsArray = [];  
@@ -31,11 +26,10 @@ Parse.Cloud.define("purchase", function(request, response) {
     }
     // save all the newly created objects
     return Parse.Object.saveAll(lifeArray).then(null, function(error){
-        console.error('Request failed with response code ' + error);
          return Parse.Promise.error('3 - Sorry, an error occurred.');
     }); 
       
-  }).then(function(result) {
+  }).then(function(results) {
     
     // We have items left! Let's create our order item before 
     // charging the credit card (just to be safe).
