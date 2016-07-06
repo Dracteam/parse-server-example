e235ccd3865e10a0bd3eb9a63a8ef7e6cea8874b
dambrosini92 <dambrosini@unocad.it>
2016-07-06 18:18:48 +0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -33,7 +33,7 @@
     // charging the credit card (just to be safe).
     order = new Parse.Object('Orders');
     order.set('name', request.params.name);
-    var status = new Parse.Object("Status_id");
+    var status = new Parse.Object("User");
     status.id = request.params.user;
     order.set("client", status);
     order.set('items', request.params.items);
