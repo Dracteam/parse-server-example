e8b683561dda3cd6f4374f51e0b808501afbf44a
dambrosini92 <dambrosini@unocad.it>
2016-09-15 13:38:19 +0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -349,4 +349,74 @@ Parse.Cloud.define("makeneworder", function(request, response){
   });
 }); 
 
+Parse.Cloud.define("cloudservertest", function(request, response){
+  var order, ordercount, general;
+  Parse.Promise.as().then(function() {
+   // Query General to get the new order number
+   var itemQuery = new Parse.Query('General');
+   itemQuery.equalTo('objectId', "e50PztuHWk");
+   return itemQuery.first().then(null, function(error) {
+      return Parse.Promise.error('Error - Order Count Not Found');
+   });
+  }).then(function(result) {
+    // Increment Orders
+    general = result;
+    ordercount = result.get('total_orders'); 
+    general.increment('total_orders');
+    // Create new order
+    return general.save().then(null, function(error) {
+      console.log('Incrementing order count failed. Error: ' + error);
+      return Parse.Promise.error('Error - Imcrement not registered, Please contact Us');
+    });
+  }).then(function(result) {
+   // Proceed to create the order
+    order = new Parse.Object('Orders');
+    order.set('name', request.params.name);
+    order.set("client", request.user);
+    order.set('items', request.params.items);
+    order.set('DeliveryOn', request.params.deliverydate);
+    order.set('payment_method', request.params.payment_method);
+    order.set('Location', request.params.deliverylocation);
+    if(request.params.payment_method === 'Credit Card'){
+      order.set('Paid', true); 
+    }else{
+      order.set('Paid', false); 
+    }
+    order.set('amount', request.params.amount);
+    ordercount++;
+    order.set('number', ordercount);  
+    order.set('Completed', false);  
+    
+    var voucher = request.params.voucher;
+    if (typeof variable !== 'undefined' || variable !== null) {
+    order.set('Voucher', voucher);  
+    }
+    // Save the order
+    return order.save().then(null, function(error) {
+      console.log('Creating order object failed. Error: ' + error);
+      return Parse.Promise.error('Error - Order not placed, Please contact Us');
+    });
+  }).then(function(order) { 
+     var str = request.params.html;
+     var newstr = str.replace("myorder", ordercount);
+     // Send Mail to User
+     Parse.Cloud.httpRequest({
+        method: "POST",
+        url: "https://api:" + process.env.MAILGUN_API_KEY + "@api.mailgun.net/v2/" + process.env.MAILGUN_DOMAIN + "/messages",
+        body: { 
+            to: request.params.mail , 
+            from: 'Your Order <' + process.env.MAILGUN_SMTP_LOGIN +'>', 
+            subject: "Thank You for your Order! - Order No. " + ordercount, 
+            html: newstr
+            }}).then(null, function(error) {
+            return Parse.Promise.error('Error - Order not placed, Please contact Us');
+        });
+  }).then(function() {
+    // And we're done!
+    response.success('Success');
+  
+  }, function(error) {
+    response.error(error);
+  });
+}); 
 
