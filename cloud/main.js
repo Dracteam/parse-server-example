e47d04fbe31d21c831d803e654965cb4e34592a3
dambrosini92 <dambrosini@unocad.it>
2016-09-15 15:48:14 +0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -124,27 +124,19 @@ Parse.Cloud.define("downloaditems", function(request, response){
 });   
 
 Parse.Cloud.define("placeorder", function(request, response){
-  var order, user, ordercount, general;
+  var order, ordercount, general;
+  // Get the user and its Token
+  var user = request.user; // request.user replaces Parse.User.current()
+  var token = user.getSessionToken(); // get session token from request.user
+  
   Parse.Promise.as().then(function() {
     // Fetch current User
-   var itemQuery = new Parse.Query('User');
-   itemQuery.equalTo('objectId', request.params.user);
-   return itemQuery.first().then(null, function(error) {
-      return Parse.Promise.error('Error - User Not Found');
-   });
-      
-  }).then(function(resultuser) {
-    // Create the User Object
-    user = resultuser;
-    // The User exists, so get the order number
-    var itemQuery = new Parse.Query('General');
+   var itemQuery = new Parse.Query('General');
    itemQuery.equalTo('objectId', "e50PztuHWk");
    return itemQuery.first().then(null, function(error) {
       return Parse.Promise.error('Error - Order Count Not Found');
    });
-
   }).then(function(result) {
-   
     // Increment Orders
     general = result;
     ordercount = result.get('total_orders'); 
@@ -154,8 +146,6 @@ Parse.Cloud.define("placeorder", function(request, response){
       console.log('Creating order object failed. Error: ' + error);
       return Parse.Promise.error('Error - Order not registered, Please contact Us');
     });
-
-  
   }).then(function(result) {
    // Proceed to create the order
     order = new Parse.Object('Orders');
@@ -165,28 +155,19 @@ Parse.Cloud.define("placeorder", function(request, response){
     order.set('DeliveryOn', request.params.deliverydate);
     order.set('payment_method', request.params.payment_method);
     order.set('Location', request.params.deliverylocation);
-    if(request.params.payment_method === 'Credit Card'){
-      order.set('Paid', true); 
-    }else{
-      order.set('Paid', false); 
-    }
+    order.set('Paid', false); 
     order.set('amount', request.params.amount);
     ordercount++;
     order.set('number', ordercount);  
     order.set('Completed', false);  
-    
-    var voucher = request.params.voucher;
-    if (typeof variable !== 'undefined' || variable !== null) {
-    order.set('Voucher', voucher);  
+    if (typeof request.params.voucher !== 'undefined') {
+    order.set('Voucher', request.params.voucher);  
     }
-    
-    // Create new order
-    return order.save().then(null, function(error) {
+    // Save the new order
+    return order.save({ sessionToken: token }).then(null, function(error) {
       console.log('Creating order object failed. Error: ' + error);
       return Parse.Promise.error('Error - Order not placed, Please contact Us');
     });
-
-  
   }).then(function(order) { 
      // Send Mail to User
      Parse.Cloud.httpRequest({
@@ -259,27 +240,19 @@ Parse.Cloud.define("increasecount", function(request, response){
 }); 
 
 Parse.Cloud.define("makeneworder", function(request, response){
-  var order, user, ordercount, general;
+  var order, ordercount, general;
+  // Get the user and its Token
+  var user = request.user; // request.user replaces Parse.User.current()
+  var token = user.getSessionToken(); // get session token from request.user
+  
   Parse.Promise.as().then(function() {
     // Fetch current User
-   var itemQuery = new Parse.Query('User');
-   itemQuery.equalTo('objectId', request.params.user);
-   return itemQuery.first().then(null, function(error) {
-      return Parse.Promise.error('Error - User Not Found');
-   });
-      
-  }).then(function(resultuser) {
-    // Create the User Object
-    user = resultuser;
-    // The User exists, so get the order number
-    var itemQuery = new Parse.Query('General');
+   var itemQuery = new Parse.Query('General');
    itemQuery.equalTo('objectId', "e50PztuHWk");
    return itemQuery.first().then(null, function(error) {
       return Parse.Promise.error('Error - Order Count Not Found');
    });
-
   }).then(function(result) {
-   
     // Increment Orders
     general = result;
     ordercount = result.get('total_orders'); 
@@ -289,8 +262,6 @@ Parse.Cloud.define("makeneworder", function(request, response){
       console.log('Creating order object failed. Error: ' + error);
       return Parse.Promise.error('Error - Order not registered, Please contact Us');
     });
-
-  
   }).then(function(result) {
    // Proceed to create the order
     order = new Parse.Object('Orders');
@@ -300,28 +271,19 @@ Parse.Cloud.define("makeneworder", function(request, response){
     order.set('DeliveryOn', request.params.deliverydate);
     order.set('payment_method', request.params.payment_method);
     order.set('Location', request.params.deliverylocation);
-    if(request.params.payment_method === 'Credit Card'){
-      order.set('Paid', true); 
-    }else{
-      order.set('Paid', false); 
-    }
+    order.set('Paid', false); 
     order.set('amount', request.params.amount);
     ordercount++;
     order.set('number', ordercount);  
     order.set('Completed', false);  
-    
-    var voucher = request.params.voucher;
-    if (typeof variable !== 'undefined' || variable !== null) {
-    order.set('Voucher', voucher);  
+    if (typeof request.params.voucher !== 'undefined') {
+    order.set('Voucher', request.params.voucher);  
     }
-    
     // Create new order
-    return order.save().then(null, function(error) {
+    return order.save({ sessionToken: token }).then(null, function(error) {
       console.log('Creating order object failed. Error: ' + error);
       return Parse.Promise.error('Error - Order not placed, Please contact Us');
     });
-
-  
   }).then(function(order) { 
      var str = request.params.html;
      var newstr = str.replace("myorder", ordercount);
@@ -349,106 +311,5 @@ Parse.Cloud.define("makeneworder", function(request, response){
   });
 }); 
 
-Parse.Cloud.define("cloudservertest", function(request, response){
-  var order, ordercount, general;
-  
-  var user = request.user; // request.user replaces Parse.User.current()
-  var token = user.getSessionToken(); // get session token from request.user
-  
-  Parse.Promise.as().then(function() {
-   // Query General to get the new order number
-   var itemQuery = new Parse.Query('General');
-   itemQuery.equalTo('objectId', "e50PztuHWk");
-   return itemQuery.first().then(null, function(error) {
-      return Parse.Promise.error('Error - Order Count Not Found');
-   });
-  }).then(function(result) {
-    // Increment Orders
-    general = result;
-    ordercount = result.get('total_orders'); 
-    general.increment('total_orders');
-    // Create new order
-    return general.save().then(null, function(error) {
-      console.log('Incrementing order count failed. Error: ' + error);
-      return Parse.Promise.error('Error - Imcrement not registered, Please contact Us');
-    });
-  }).then(function(result) {
-   // Proceed to create the order
-    order = new Parse.Object('Orders');
-    order.set('name', request.params.name);
-    order.set("client", user);
-    order.set('items', request.params.items);
-    order.set('DeliveryOn', request.params.deliverydate);
-    order.set('payment_method', request.params.payment_method);
-    order.set('Location', request.params.deliverylocation);
-    if(request.params.payment_method === 'Credit Card'){
-      order.set('Paid', true); 
-    }else{
-      order.set('Paid', false); 
-    }
-    order.set('amount', request.params.amount);
-    ordercount++;
-    order.set('number', ordercount);  
-    order.set('Completed', false);  
-    
-    // Save the order
-    return order.save({ sessionToken: token }).then(null, function(error) {
-      console.log('Creating order object failed. Error: ' + error);
-      return Parse.Promise.error('Error - Order not placed, Please contact Us');
-    });
-  }).then(function(order) { 
-     var str = request.params.html;
-     var newstr = str.replace("myorder", ordercount);
-     // Send Mail to User
-     Parse.Cloud.httpRequest({
-        method: "POST",
-        url: "https://api:" + process.env.MAILGUN_API_KEY + "@api.mailgun.net/v2/" + process.env.MAILGUN_DOMAIN + "/messages",
-        body: { 
-            to: request.params.mail , 
-            from: 'Your Order <' + process.env.MAILGUN_SMTP_LOGIN +'>', 
-            subject: "Thank You for your Order! - Order No. " + ordercount, 
-            html: newstr
-            }}).then(null, function(error) {
-            return Parse.Promise.error('Error - Order not placed, Please contact Us');
-        });
-  }).then(function() {
-    // And we're done!
-    response.success('Success');
-  
-  }, function(error) {
-    response.error(error);
-  });
-}); 
 
-Parse.Cloud.define("onlyorder", function(request, response){
-  var order;
-  var user = request.user; // request.user replaces Parse.User.current()
-  var token = user.getSessionToken(); // get session token from request.user
-  
-  Parse.Promise.as().then(function() {
-   // Proceed to create the order
-    order = new Parse.Object('Orders');
-    order.set('name', request.params.name);
-    order.set("client", user);
-    order.set('items', request.params.items);
-    order.set('DeliveryOn', request.params.deliverydate);
-    order.set('payment_method', request.params.payment_method);
-    order.set('Location', request.params.deliverylocation);
-    order.set('Paid', false); 
-    order.set('amount', request.params.amount);
-    order.set('Completed', false);  
-    if (typeof request.params.voucher !== 'undefined') {
-    order.set('Voucher', request.params.voucher);  
-    }
-    // Save the order
-    return order.save({ sessionToken: token }).then(null, function(error) {
-      console.log('Creating order object failed. Error: ' + error);
-      return Parse.Promise.error('Error - Order not placed, Please contact Us');
-    });
-  }).then(function() {
-    // And we're done!
-    response.success('Success');
-  }, function(error) {
-    response.error(error);
-  });
-}); 
+
